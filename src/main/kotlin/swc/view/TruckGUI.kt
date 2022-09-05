0af99d828da55e113da4c9578b0b9917422177d8
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2022-09-05 17:10:53 +0200

    chore: add truck choice and display info

--- src/main/kotlin/swc/view/TruckGUI.kt
+++ src/main/kotlin/swc/view/TruckGUI.kt
@@ -1,26 +1,62 @@
 package swc.view
 
+import swc.drivers.Operations
+import swc.entities.Truck
 import java.awt.BorderLayout
 import java.awt.Dimension
+import java.awt.FlowLayout
+import java.awt.GridLayout
+import javax.swing.BoxLayout
+import javax.swing.JButton
 import javax.swing.JFrame
 import javax.swing.JLabel
 import javax.swing.JPanel
+import javax.swing.JTextField
+import javax.swing.SwingUtilities
 
 object TruckGUI {
     class TruckFrame(title: String) : JFrame() {
         init {
             this.title = title
-            this.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+            this.defaultCloseOperation = EXIT_ON_CLOSE
             this.size = Dimension(800, 600)
             this.setLocationRelativeTo(null)
-            this.add(TruckPanel(this))
+            this.add(SelectTruckPanel(this))
         }
     }
 
-    class TruckPanel(frame: JFrame) : JPanel() {
+    class SelectTruckPanel(frame: JFrame) : JPanel() {
+        private val truckId: JTextField = JTextField(20)
+        private val selectTruck: JButton = JButton("Select Truck")
+
         init {
-            this.layout = BorderLayout()
-            this.add(JLabel("Truck Panel"), BorderLayout.NORTH)
+            this.layout = FlowLayout()
+            this.add(JLabel("Truck ID:"))
+            this.add(truckId)
+            this.selectTruck.addActionListener {
+                Operations.getTruck(this.truckId.text).thenApply {
+                    SwingUtilities.invokeLater {
+                        println("Truck: $it")
+                        frame.remove(this)
+                        TruckPanel(frame, it)
+                    }
+                }
+            }
+            this.add(selectTruck)
+            frame.add(this)
+        }
+    }
+
+    class TruckPanel(frame: JFrame, truck: Truck) : JPanel() {
+        init {
+            this.layout = BoxLayout(this, BoxLayout.Y_AXIS)
+            this.add(JLabel("Id: ${truck.truckId}"))
+            this.add(JLabel("Position: (${truck.position.latitude}, ${truck.position.longitude})"))
+            this.add(JLabel("Occupied volume: ${truck.occupiedVolume.value}L"))
+            this.add(JLabel("Capacity: ${truck.capacity}L"))
+            this.add(JLabel("In mission: ${truck.isInMission}"))
+            frame.add(this)
+            frame.isVisible = true
         }
     }
 
